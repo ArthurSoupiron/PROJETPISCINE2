564326a9265c9bf17fc236ed93b5bd22232291b3
ArthurSoupiron <149239875+ArthurSoupiron@users.noreply.github.com>
2025-06-01 17:16:43 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ $db = 'sportify';
 $user = 'root';
 $pass = ''; // Mets ton mot de passe si tu en as un
 
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,3 +12,8 @@ try {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
 ?>
+
+
+
+
+
