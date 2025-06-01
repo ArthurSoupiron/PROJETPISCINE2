837a0cf88cdc647e64a0dd8524d01aefb1f1aa87
LucasYng1 <149158164+LucasYng1@users.noreply.github.com>
2025-06-01 22:46:16 +0200

    Update traitement_connexion.php

--- traitement_connexion.php
+++ traitement_connexion.php
@@ -2,23 +2,19 @@
 session_start();
 require_once('db.php');
 
-// Vérifier que les champs sont bien remplis
+// on verifie que tous les champs sont bien remplis
 if (isset($_POST['email']) && isset($_POST['mot_de_passe'])) {
     $email = $_POST['email'];
     $mot_de_passe = $_POST['mot_de_passe'];
 
-    // Rechercher l'utilisateur avec l'email
+    // on recherche l'utilisateur avec l'email
     $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
     $stmt->execute([$email]);
     $user = $stmt->fetch();
 
-    // Vérifier si un utilisateur est trouvé et si le mot de passe est correct
+    // cela nous permet de verifier  si un utilisateur est trouvé et si le mot de passe est correct
     if ($user && password_verify($mot_de_passe, $user['mot_de_passe'])) {
-        // Stocker les infos utilisateur dans la session
-        // Stocker toutes les infos dans 'user' pour un accès global
         $_SESSION['user'] = $user;
-
-// Et aussi dans des variables séparées si nécessaire
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['email'] = $user['email'];
         $_SESSION['role'] = $user['role'];
@@ -26,7 +22,7 @@ if (isset($_POST['email']) && isset($_POST['mot_de_passe'])) {
         $_SESSION['prenom'] = $user['prenom'];
 
 
-        // Rediriger vers la page d'accueil
+        // permet de nous rediriger vers la page d'accueil
         header("Location: index.php");
         exit();
     } else {
